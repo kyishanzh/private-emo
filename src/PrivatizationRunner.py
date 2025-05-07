6648552b678b483a6b4b90ad47aaa382cf6d96f1
Jacqueline Wang <jacquelineyujia@gmail.com>
2025-05-06 20:35:50 -0400

    quantization

--- src/PrivatizationRunner.py
+++ src/PrivatizationRunner.py
@@ -1,14 +1,18 @@
 import os
 from pathlib import Path
 import cv2
+from PIL import Image
 from typing import Callable
+
+VALID_EXTENSIONS = {".jpg", ".jpeg", ".png", ".bmp", ".tiff"}
 
 class PrivatizationRunner():
 
-    def __init__(self, in_path : str, out_path : str, privatization) -> None:
+    def __init__(self, in_path : str, out_path : str, privatization, useCV : False) -> None:
         self.in_path = in_path
         self.out_path = out_path
         self.privatization = privatization
+        self.useCV = useCV
 
     def run(self, debug = False) -> None:
         for root, _, files in os.walk(self.in_path):
@@ -16,8 +20,12 @@ class PrivatizationRunner():
             for file in files:
                 # get input image
                 file_path = root_path / file
+                if file_path.suffix.lower() not in VALID_EXTENSIONS:
+                    if debug:
+                        print(f"Skipping non-image file: {file_path}")
+                    continue
                 # process
-                out_img = self.privatization(str(file))
+                out_img = self.privatization(str(file_path))
                 # handle output file path stuff
                 rel_path = file_path.relative_to(self.in_path)
                 new_filename = f"{file_path.stem}_out{file_path.suffix}"
@@ -26,13 +34,16 @@ class PrivatizationRunner():
                 output_file = output_subdir / new_filename
 
                 # Save processed image
-                cv2.imwrite(str(output_file), out_img)
+                if self.useCV:
+                    cv2.imwrite(str(output_file), out_img)
+                else:
+                    out_img.save(output_file)
                 if debug:
                     print(f"Processed and saved: {output_file}")
 
 
-cwd = os.getcwd()
-print("Current working directory:", cwd)
+# cwd = os.getcwd()
+# print("Current working directory:", cwd)
 
 # our input directories
 celebA_dir = "CelebrityFacesDataset"
